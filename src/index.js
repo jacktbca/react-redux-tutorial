9fe106505b242ed4367f1db795e994e0d3ee1a5c
Jack Lo <jack.lo@tbcasoft.com>
2020-08-13 17:32:56 +0800

    Connect react with redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import index from "./js/index";
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from "./js/components/App";
 
 // Use npm start to run the application.
 
@@ -9,3 +13,10 @@ import index from "./js/index";
 //   $ store.dispatch( addArticle({ title: 'React Redux Tutorial for Beginners', id: 1}))
 // 3. check the latest state 
 //   $ store.getState()
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
