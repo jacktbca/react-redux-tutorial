ac94d8f59899616a522bcbb86d53e0db2f835c1e
Jack Lo <jack.lo@tbcasoft.com>
2020-08-13 13:46:34 +0800

    Make Redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import index from "./js/index";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Use npm start to run the application.
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// After you do that, you could use 
+// 1. The subscribe method accepts a callback that will fire whenever an action is dispatched.
+//   $ store.subscribe(() => console.log('Look ma, Redux!!'));
+// 2. Use dispatch method to dispatch an action
+//   $ store.dispatch( addArticle({ title: 'React Redux Tutorial for Beginners', id: 1}))
+// 3. check the latest state 
+//   $ store.getState()
